6a6f73b283519f054ba58a91b08b26678ac54485
Rachel Anne Cilon <rachelannecilon@gmail.com>
2024-12-12 14:28:11 +0800

    update your-emotion.c

--- your-emotion.c
+++ your-emotion.c
@@ -3,7 +3,7 @@
 int main(int argc, const char * argv[])
 {
 	char your_emotion[50]; 
-	printf("How are you feeling today? (happy or sad) ");
+	printf("How are you feeling today? (don't use pronoun, e.g. sad) ");
 	scanf("%49s", &your_emotion);
 	
 	printf("You are feeling %s today!\n", your_emotion);
