5fbeacf167ee47b2e1a3a35def71f28556af0031
Rachel Anne Cilon <113764045+rachelannec@users.noreply.github.com>
2025-01-20 13:22:33 +0800

    Update main.c

--- library-practice/main.c
+++ library-practice/main.c
@@ -3,30 +3,22 @@
 
 int main(){
 	greet();
-
+	
 	int a = 5, b = 3, c = 49;
-
+	
 	int q = add(a, b);
 	int w = subtract(a, b);
-    int e = multiply(a, b);
-    float r = divide(a, b);
-    int t = square(a);
-    double y = squareRoot(c);
-
-    printf("Addition: %d\n", q);
-    printf("Subtraction: %d\n", w);
-    printf("Multiplication: %d\n", e);
-    printf("Division: %f\n", r);
-    printf("Square: %d\n", t);
-    printf("Square Root: %.1lf\n", y);
-
+	int e = multiply(a, b);
+	float r = divide(a, b);
+	int t = square(a);
+	double y = squareRoot(c);
 	
-	// printf("%d", add(2, 5));
-	// printf("%d", subtract(5, 2));
-	// printf("%d", multiply(2, 5));
-	// printf("%f", divide(5, 2));
-	// printf("%d", square(5));
-	// printf("%d", squareRoot(49));
+	printf("Addition: %d\n", q);
+	printf("Subtraction: %d\n", w);
+	printf("Multiplication: %d\n", e);
+	printf("Division: %f\n", r);
+	printf("Square: %d\n", t);
+	printf("Square Root: %.1lf\n", y);
 
 	return 0;
 }
@@ -39,4 +31,4 @@ int main(){
 // Multiplication: 15
 // Division: 1.666667
 // Square: 25
-// Square Root: 7
+// Square Root: 7.0
