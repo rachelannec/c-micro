6c948aef8777948ba9eab2ed324e990a5bdd422e
Rachel Anne Cilon <annerachel095@gmail.com>
2025-06-04 14:56:54 +0800

    another one

--- struct/discu2-429.c
+++ struct/discu2-429.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+#define MAX 3
+
 struct Student{
 	char name[80];
 	int age;
@@ -7,25 +9,25 @@ struct Student{
 };
 
 int main(){
-	struct Student st[3];
+	struct Student st[MAX];
 	
 	printf("Detail for %d students: \n\n", MAX);
-	for (int i = 0; i< 3; i++){
+	for (int i = 0; i< MAX; i++){
 		printf("Student %d\n", i+1);
 		printf("Name: ");
-		scanf(" %[^\n]", Student[i].name); // string with spaces
+		scanf(" %[^\n]", st[i].name); // string with spaces
 		printf("Age: ");
-		scanf(" %f", &Student[i].age);
+		scanf(" %d", &st[i].age);
 		printf("Gender: ");
-		scanf(" %c", &Student[i].gender);
+		scanf(" %c", &st[i].gender);
 		
 		printf("\n\n");
 	}
 	
 	// printing
 	printf("List of students: \n");
-	for (int i=0; i< 3; i++){
-		printf("Name: %s, Grade: %.2f\n", Student[i].name, Student[i].gender);
+	for (int i=0; i< MAX; i++){
+		printf("Name: %s, Age: %d, Gender: %c\n", st[i].name, st[i].age, st[i].gender);
 	}
 	
 	return 0;
